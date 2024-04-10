558dd6bd1ffd6c842d6f14f3f5ffec11ae9ce163
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2024-04-10 18:22:13 +0200

    Updated code for testing

--- postSystem/postSystem/Program.cs
+++ postSystem/postSystem/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using postSystem;
+
 Console.WriteLine("Hello, World!");
+
+List<Item>? result = JsonReader.ReadItems();
+
+if (result != null)
+{
+    foreach (Item item in result)
+    {
+        Console.WriteLine($"Description: {item.Description}, Dimensions: [{string.Join(", ", item.Dimensions)}], Weight: {item.Weight}");
+    }
+}
