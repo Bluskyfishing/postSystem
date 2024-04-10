3bbabc519bf9389c005a9bffea515925314b4a6d
Bluskyfishing <blusky445@gmail.com>
2024-04-10 14:44:29 +0200

    Implemented packingfinder method.

---
+++ postSystem/postSystem/Postage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postSystem
+{
+    public static class Postage
+    {
+        public static int[] PackingFinder(int[] dimentions) 
+        {
+            int length = dimentions[0];
+            int width = dimentions[1];
+            int height = dimentions[2];
+
+            if (height < 20)
+            {
+                int[] option1 = [110, 160];
+                int[] option2 = [150, 210];
+                int[] option3 = [180, 260];
+                int[] option4 = [270, 360];
+                int[] option5 = [350, 470];
+
+                List<int[]> packages = new List<int[]> { option1, option2, option3, option4, option5 };
+
+                foreach (int[] package in packages)
+                {
+                    if (package[0] >= length && package[1] >= width)
+                    {
+                        return package;
+                    }
+                }
+                return [1, 0, 0];
+            }
+
+            else if (height > 20) 
+            {
+                //length, width, height, weight
+                int[] mini = [240, 159, 60, 67];
+                int[] liten = [332, 246, 65, 126]; //Rounded from 125,5 to 126
+                int[] eskeNorgesPakken = [350, 250, 120, 191];
+                int[] stor = [500, 300, 200, 359];
+
+                List<int[]> packages = new List<int[]> { mini, liten, eskeNorgesPakken, stor };
+
+                foreach (int[] package in packages)
+                {
+                    if (package[0] >= length && package[1] >= width && package[2] >= height)
+                    {
+                        return package;
+                    }
+                }
+                return [2,0,0];
+            }
+            return [0,0,0];
+        }
+    }
+    
+}
