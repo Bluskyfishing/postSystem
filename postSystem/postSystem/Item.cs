435a604d49f5787d00cf6f80ec0e19cf648f39ab
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2024-04-10 17:57:32 +0200

    Added JsonReader.cs and Item.cs

---
+++ postSystem/postSystem/Item.cs
@@ -0,0 +1,8 @@
+namespace postSystem;
+
+public class Item(string description, int[] dimensions, int weight)
+{
+    public string Description = description;
+    public int[] Dimensions = dimensions;
+    public int Weight = weight;
+}
