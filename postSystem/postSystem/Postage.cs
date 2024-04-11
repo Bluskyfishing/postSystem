568671e82b406e2d4a2088143bd7122aa7bccb59
Bluskyfishing <blusky445@gmail.com>
2024-04-11 17:58:51 +0200

    added the different postage options.

--- postSystem/postSystem/Postage.cs
+++ postSystem/postSystem/Postage.cs
@@ -11,12 +11,17 @@ namespace postSystem
 {
     public static class Postage
     {
-        public static string Postagefinder(int[] packingType, int[] packageDimensions, int itemWeight)
+        //Expected output: ["Item Description", "Weight", "Package type, "Package price",  "Postage type", "Postage Price"] 
+        public static string[] Postagefinder(string itemDescription, int[] packingType, int[] packageDimensions, int itemWeight)
         {
+            string[] totalItemsArray = new string[6];
+            totalItemsArray[0] = itemDescription;
+            totalItemsArray[1] = itemWeight.ToString();
+
             int length = packingType[0]; //Length of box/letter
             int width = packingType[1]; //Width of of box/letter
 
-            if (packingType.Length > 2) //Packages Options
+            if (packingType.Length > 3) //Packages Options
             {
                 int height = packingType[2]; //Height of of box only
                 int packageWeight = packingType[3]; //Weight of box only 
@@ -25,17 +30,52 @@ namespace postSystem
                 //Norgespakke liten (Inntil 5 kg)
                 if (length <= 350 && width <= 250 && height <= 12 && weight >= 350 || weight <= 5000)
                 {
-                    Console.WriteLine();
-                }
+                    string postageTypeName = "Norgespakke small (up to 5 kg)";
+                    totalItemsArray[2] = postageTypeName;
 
+                    int price = 73;
+                    totalItemsArray[3] = price.ToString();
+
+                    return totalItemsArray;
+
+                }
                 //Norgespakke stor (over 2kg)
                 else if (length > 350 && width > 250 && height > 12 && weight > 2000 && length <= 1200 && width > 600 && height > 600)
                 {
-                    Console.WriteLine();
+                    string postageTypeName = "Norgespakke big (over 2kg)";
+                    totalItemsArray[2] = postageTypeName;
+
+                    int[] option1 = [10000, 177]; //Will be handed in at posten so doesnt need price for posten.no?
+                    int[] option2 = [25000, 293];
+                    int[] option3 = [35000, 377];
+
+                    List<int[]> postageOptions = new List<int[]> { option1, option2, option3 };
+
+                    foreach (int[] option in postageOptions)
+                    {
+                        if (weight <= 10000)
+                        {
+                            totalItemsArray[3] = option[1].ToString();
+                            return totalItemsArray;
+                        }
+                        else if (weight <= 25000)
+                        {
+                            totalItemsArray[3] = option[1].ToString();
+                            return totalItemsArray;
+                        }
+                        else if (weight <= 35000)
+                        {
+                            totalItemsArray[3] = option[1].ToString();
+                            return totalItemsArray;
+                        }
+                    }
+
                 }
                 else
                 {
-                    return "Package is too big!";
+                    totalItemsArray[2] = "Too big to ship!";
+                    totalItemsArray[3] = "NO PRICE";
+                    return totalItemsArray;
                 }
             }
             else //Letters Options
@@ -45,26 +85,85 @@ namespace postSystem
                 //Letter under 350g
                 if (length <= 350 && width <= 250 && height <= 70 && itemWeight < 350) 
                 {
-                    Console.WriteLine();
+                    string postageTypeName = "Letter under 350g";
+                    totalItemsArray[2] = postageTypeName;
+
+                    // array = [weight, 2cm height price, 2-7cm height price]
+                    int[] option1 = [20, 23, 57];
+                    int[] option2 = [50, 29, 57];
+                    int[] option3 = [100, 36, 57];
+                    int[] option4 = [350, 55, 57];
+
+                    List<int[]> postageOptions = new List<int[]> { option1, option2, option3, option4};
+
+                    foreach (int[] option in postageOptions)
+                    {
+                        if (option[0] >= itemWeight)
+                        {
+                            if (height <= 20) //Inntil 2 cm tykk 	
+                            {
+                                totalItemsArray[3] = option[1].ToString();
+                                return totalItemsArray;
+                            }
+                            else //2–7 cm tykk
+                            {
+                                totalItemsArray[3] = option[1].ToString();
+                                return totalItemsArray;
+                            }
+                        }
+                    }
+
                 }
                 //Brev fra 350g til 2kg 
                 else if (length + width + height <= 900 && length < 600 && width < 600 && height < 600 && itemWeight >= 350 || itemWeight <= 2000)
                 {
-                    Console.WriteLine();
+                    string postageTypeName = "Letter from 350g to 2kg ";
+                    totalItemsArray[2] = postageTypeName;
+
+                    // array = [weight, 2cm height price, 2-7cm height price, More than 7cm height price]
+                    int[] option1 = [20, 23, 57, 90];
+                    int[] option2 = [50, 29, 57, 90];
+                    int[] option3 = [100, 36, 57, 90];
+                    int[] option4 = [350, 55, 57, 90];
+                    int[] option5 = [1000, 90, 105, 140];
+                    int[] option6 = [2000, 125, 135, 175];
+
+                    List<int[]> postageOptions = new List<int[]> { option1, option2, option3, option4, option5, option6 };
+
+                    foreach (int[] option in postageOptions)
+                    {
+                        if (option[0] >= itemWeight)
+                        {
+                            if (height >= 20)
+                            {
+                                totalItemsArray[3] = option[1].ToString();
+                                return totalItemsArray;
+                            }
+                            else if (height <= 20)
+                            {
+                                totalItemsArray[3] = option[1].ToString();
+                                return totalItemsArray;
+                            }
+                            else if (height > 70)
+                            {
+                                totalItemsArray[3] = option[1].ToString();
+                                return totalItemsArray;
+                            }
+                        }
+                    }
+
                 }
                 else 
                 {
-                    return "Package is too big!";
+                    totalItemsArray[2] = "Too big to ship!";
+                    totalItemsArray[3] = "NO PRICE";
+                    return totalItemsArray;
                 }
 
             }
 
-            //Description,
-            //Total Weight per item(item+container),
-            //Price(per shipped object),
-            //Total price. 
-
-            return "work in progress";
+            //Expected output: ["Item Description", "Weight", "Package type, "Package price",  "Postage type", "Postage Price"] 
+            return totalItemsArray;
         }
     }
 }
