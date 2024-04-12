acb7664445883b11fc7c2030ce1cc96020b4a9ee
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2024-04-12 15:00:41 +0200

    Added some comments to Program.cs

--- postSystem/postSystem/Program.cs
+++ postSystem/postSystem/Program.cs
@@ -2,31 +2,35 @@
 using postSystem.readData;
 using System.Runtime.ConstrainedExecution;
 
-List<Item>? result = JsonReader.ReadItems();
+// Extract the contents from the .json file
+List<Item>? fileContents = JsonReader.ReadItems();
 
-if (result != null)
+// 
+if (fileContents != null)
 {
     List<string[]> allItems = new List<string[]>();
 
-    foreach (Item item in result)
+    foreach (Item item in fileContents)
     {
         float[] packing = Packing.PackingFinder(item.Dimensions);
         string[] postage = Postage.Postagefinder(item.Description, packing, item.Dimensions, item.Weight);
         allItems.Add(postage);
     }
+    
+    // Write the list to the user
     Console.WriteLine("Shipping list:\n" +
                       "Item Description : Weight : Package type : Package price :  Postage type : Postage Price\n");
-
     foreach (string[] postage in allItems)
     {
         Console.WriteLine(string.Join(" : ",postage));
     }
     
+    // write the sum list to the user
     List<float> sum = SumCalculator.CalculateSum(allItems);
-    
     Console.WriteLine($"\nQuantity of items: {sum[0]}\n" +
                       $"Total weight: {sum[1]} g\n" +
                       $"Total price: {sum[2]},-\n");
     
+    // Saves the file if the user chooses to
     FileWriter.WriteFile(allItems, sum);
 }
